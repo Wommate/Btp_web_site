f898713fe59674a17d0458896e6dbfac6c32a7ba
Demba-Diakhate <diakhatedemba@gmail.com>
2024-12-13 12:25:02 +0000

    Add Navbar Link

---
+++ frontend/src/components/about/header.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <div className="backHeader mb-10">
+       <div className='w-full h-full bg-black/30'>
+            
+       </div>
+    </div>
+  )
+}
+
+export default Header
