80cd114d4feb2546780c7bf6fbeddafaadd53e23
Bakary <bkrsagna03@gmail.com>
2024-12-13 11:15:33 +0000

    pull the update to get the pages of the project

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import Contact from './components/contact/Contact';
+import FAQ from './components/faq/FAQ';
 import Footer from './components/footer/Footer';
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h1>Btp WebSite</h1>
+        <FAQ />
         <Contact />
         <Footer />
       </header>
--- frontend/src/components/contact/Contact.jsx
+++ frontend/src/components/contact/Contact.jsx
@@ -17,7 +17,7 @@ const Contact = () => {
                         <p className='md:text-xl text-base text-white text-left font-bold'>+221 78 235 45 70</p>
                     </div>
                 </div>
-                <div className='absolute md:top-[47%] top-[37%] md:left-[22%] left-[40%] bg-[#fcd019] p-4 w-16 h-[58px] rounded-b-full'></div>
+                {/* <div className='absolute md:top-[727px] top-[85%] md:left-[22%] left-[40%] bg-[#fcd019] p-4 w-16 h-[58px] rounded-b-full'></div> */}
                 <div className='background-contact-2 flex gap-3 shadow p-6 rounded-2xl md:w-3/4 w-full'>
                     <LuMessageSquareText className='font-bold md:text-5xl my-2 text-5xl bg-[#35c6f4] text-white rounded-xl p-3' />
                     <div>
@@ -25,7 +25,7 @@ const Contact = () => {
                         <p className='md:text-xl text-white text-base text-left font-bold'>etep02@gmail.com</p>
                     </div>
                 </div>
-                <div className='absolute md:bottom-[105px] bottom-[355px] md:left-[22%] left-[40%] bg-[#fcd019] p-4 w-16 h-[58px] rounded-b-full'></div>
+                {/* <div className='absolute md:-bottom-[57%] -bottom-[8%] md:left-[22%] left-[40%] bg-[#fcd019] p-4 w-16 h-[58px] rounded-b-full'></div> */}
                 <div className='background-contact-3 flex gap-3 shadow p-6 rounded-2xl md:w-3/4 w-full'>
                     <GiPositionMarker className='font-bold md:text-5xl text-5xl my-2 bg-[#35c6f4] text-white rounded-xl p-3' />
                     <div>
@@ -38,24 +38,19 @@ const Contact = () => {
                 <form action="#" method="post" className='flex flex-col gap-5 justify-center'>
                     <div className="flex flex-row gap-3">
                         <div className='flex flex-col gap-1 w-1/2'>
-                            {/* <label className='md:text-xl text-gray-600 opacity-60 text-left font-semibold' htmlFor="name">Name</label> */}
                             <input className='border rounded-lg p-1 placeholder:text-lg placeholder:px-2' type="text" name="name" id="name" placeholder='Nom' />
                         </div>
                         <div className='flex flex-col gap-1 w-1/2'>
-                            {/* <label className='md:text-xl text-gray-600 opacity-60 text-left font-semibold' htmlFor="email">Email</label> */}
                             <input className='border rounded-lg p-1 placeholder:text-lg placeholder:px-2' type="email" name="email" id="email" placeholder='Email' />
                         </div>
                     </div>
                     <div className='flex flex-col gap-1'>
-                        {/* <label className='md:text-xl text-gray-600 opacity-60 text-left font-semibold' htmlFor="subject">Subject</label> */}
                         <input className='border rounded-lg p-1 placeholder:text-lg placeholder:px-2' type="text" name="subject" id="subject" placeholder='Objet' />
                     </div>
                     <div className='flex flex-col gap-1'>
-                        {/* <label className='md:text-xl text-gray-600 opacity-60 text-left font-semibold' htmlFor="phone">Phone</label> */}
                         <input className='border rounded-lg p-1 placeholder:text-lg placeholder:px-2' type="tel" name="phone" id="phone" placeholder='Tel' />
                     </div>
                     <div className='flex flex-col gap-1'>
-                        {/* <label className='md:text-xl text-gray-600 opacity-60 text-left font-semibold' htmlFor="content">Content</label> */}
                         <textarea name="content" id="content" rows="10" className='border rounded-lg p-1 h-44 placeholder:text-lg placeholder:px-2' placeholder='Ecrivez votre message'></textarea>
                     </div>
                     <input type="submit" value="Soumettre" className='p-3 rounded-lg bg-[#35c6f4] text-black text-lg mt-3 font-bold md:w-1/4 w-1/2 mx-auto' />
