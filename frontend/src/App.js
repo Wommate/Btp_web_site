848f6bee62c85da7334c25917482704147db0970
odia221 <senbusinessfk@gmail.com>
2024-12-16 13:33:43 +0100

    merged

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,3 @@
-<<<<<<< HEAD
-import './App.css';
-import Navbar_sup from './components/navbar_sup/navbar_sup'
-import Navbar from './components/navbar/navbar';
-import Hero_section from './components/hero_section/hero_section';
-import Contact from './components/contact/Contact';
-import Footer from './components/footer/Footer';
-
-function App() {
-  return (
-    <>
-      <div className="">      
-        <Navbar />
-        <Hero_section />
-        <header className="App App-header">
-          <Contact />
-          <Footer />
-        </header>
-      </div>
-    </>
-=======
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/home";
@@ -38,7 +17,6 @@ function App() {
       <Route path="/contact" element={<Contact />} />
       <Route path="/faq" element={<Faq />} />
     </Routes>
->>>>>>> f898713fe59674a17d0458896e6dbfac6c32a7ba
   );
 }
 
