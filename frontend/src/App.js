d00de12d29bf99609b62e49a561c1da071a3747b
odia221 <senbusinessfk@gmail.com>
2024-12-18 22:42:49 +0100

    dashbord ok

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,21 @@
 import './App.css';
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+// import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Dashboard from './pages/dashboard/dashboard/dashboard';
-import BlogForm from './pages/dashboard/blog/blog';
-import GalleryForm from './pages/dashboard/gallery/gallery';  
+// import BlogForm from './pages/dashboard/blog/blog';
+// import GalleryForm from './pages/dashboard/gallery/gallery';  
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Dashboard />} />
-        <Route path="/blog" element={<BlogForm />} />
-        <Route path="/gallery" element={<GalleryForm />} />
-      </Routes>
-    </Router>
+    <Dashboard />
+    // <Router>
+    //   <Routes>
+        
+    //     <Route path="/" element={<Dashboard />} />
+    //     <Route path="/blog" element={<BlogForm />} />
+    //     <Route path="/gallery" element={<GalleryForm />} />
+    //   </Routes>
+    // </Router>
   );
 }
 
--- frontend/src/pages/dashboard/dashboard/sidebar.jsx
+++ frontend/src/pages/dashboard/dashboard/sidebar.jsx
@@ -1,15 +1,16 @@
 import React from 'react';
 import './sidebar.css';
 
+
 const Sidebar = () => {
   return (
     <div className="sidebar">
-      <h2>Sidebar</h2>
+    <div className="logo-container"><img src="/img/ETEP.png" alt="Logo" className="logo" /></div>
       <ul>
         <li>Accueil</li>
         <li>Blog</li>
         <li>Gallery</li>
-        <li>Service</li>
+        <li>Services</li>
       </ul>
     </div>
   );
