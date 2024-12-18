c510bc4b0805edc90b0fb22e9a790163bbd61a82
odia221 <senbusinessfk@gmail.com>
2024-12-18 19:08:27 +0100

    tableau de bord gestion blog et gallery ok

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,19 @@
+import './App.css';
 import React from 'react';
-
-// import Dashboard from './pages/dashboard/dashboard/dashboard';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Dashboard from './pages/dashboard/dashboard/dashboard';
 import BlogForm from './pages/dashboard/blog/blog';
-// import GalleryForm from './pages/dashboard/gallery/gallery';
+import GalleryForm from './pages/dashboard/gallery/gallery';  
 
 function App() {
   return (
-    // <GalleryForm />
-    <BlogForm />
-
-    // <Dashboard />
-
+    <Router>
+      <Routes>
+        <Route path="/" element={<Dashboard />} />
+        <Route path="/blog" element={<BlogForm />} />
+        <Route path="/gallery" element={<GalleryForm />} />
+      </Routes>
+    </Router>
   );
 }
 
@@ -20,47 +23,12 @@ export default App;
 
 
 
+    // <GalleryForm />
+    // <BlogForm />
+    // <Dashboard />
 
-// import './App.css'
-// import Inscription from './pages/Inscription'
-// import Admin from './pages/Admin'
-// import Archive from './pages/Archive'
-// import Modifmdp from './pages/Modifmdp'
-// import Sante from './pages/Sante'
-// import Deplacement from './pages/Deplacement'
-// import Donneconcerne from './pages/Donneconcerne'
-// import Dashbord from './pages/Dashbord'
-// import { Route, Routes } from 'react-router-dom'
-// import Connexion from './pages/Connexion'
-//  import TB from './pages/TB'
-//  import Auth from './pages/Auth'
-//  import ProtectionRoutes from './pages/ProtectionRoutes'
+
+
  
  
-// function App() {
 
-
-//   return (
-//     <div className='pages'>
-//     <>
-//     <Routes>
-//             <Route index path="/" element={<Connexion/>} />
-//             <Route path='/Dashbord' element={<Auth><Dashbord/></Auth>}>
-//               <Route path='Archive' element={<Auth><Archive/></Auth>}/>
-//               <Route path='Admin' element={<Auth><Admin/></Auth>}/>
-//               <Route path='Deplacement' element={<Auth><Deplacement/></Auth>}/>
-//               <Route path='Inscription' element={<Auth><Inscription/></Auth>}/>
-//               <Route path='Modifmdp' element={<Auth><Modifmdp/></Auth>}/>
-//               <Route path='Donneconcerne' element={<Auth><Donneconcerne/></Auth>}/>
-//               <Route path='TB' element={<Auth>TB</Auth>}/>
-
-//            </Route>
-//            <Route path="*" element={<ProtectionRoutes />} />
-
-//     </Routes>
-//     </>
-//   </div>
-//   )
-// }
-
-// export default App
