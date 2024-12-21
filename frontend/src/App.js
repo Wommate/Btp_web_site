8af17efd31e7eefef05585ef16c10f89ff125212
odia221 <senbusinessfk@gmail.com>
2024-12-21 16:01:45 +0100

    gerging odia

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,5 @@
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
-import Home from "./pages/home";
-import About from "./pages/about";
-import Services from "./pages/services";
-import Blog from "./pages/blog";
-import Contact from "./pages/contact";
-import Faq from "./pages/faq";
-import AllGallery from "./pages/allGallery";
 
 // Import for dashboard
 import Dashboard from './pages/dashboard/dashboard/dashboard';
@@ -22,14 +15,6 @@ function App() {
     <>
       {/* Routes publiques */}
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/services" element={<Services />} />
-        <Route path="/blog" element={<Blog />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/faq" element={<Faq />} />
-        <Route path="/allGallery" element={<AllGallery />} />
-
         {/* Routes nécessitent une authentification */}
         <Route
           path="/dashboard/*"
@@ -53,12 +38,3 @@ function App() {
 }
 
 export default App;
-
-
-
-
-
-
- 
-
-
